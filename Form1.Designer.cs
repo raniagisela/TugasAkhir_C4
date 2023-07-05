8285452eca2aaed4d8a3a1fcff874e08363fe64c
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2023-07-05 18:09:11 +0700

    change text in label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,9 @@ namespace TugasAkhir_C4
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(354, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(80, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Mini Market";
             // 
             // Form1
             // 
