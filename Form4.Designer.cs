3b4ee9fd51362a78e444c5960a1fbbfccf3c9907
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2023-07-05 18:32:39 +0700

    change background data grid view

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -46,6 +46,7 @@ namespace TugasAkhir_C4
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MistyRose;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(104, 80);
             this.dataGridView1.Name = "dataGridView1";
