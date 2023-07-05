e36105e349d79eabbdcf8462896fd87b9676d76d
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2023-07-05 18:31:10 +0700

    change the back color in form 2 3 4

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -155,6 +155,7 @@ namespace TugasAkhir_C4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
